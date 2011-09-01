2587f4153d325d50979c50b2a63140af97a976dd
Ignasi Barrera <ignasi.barrera@abiquo.com>
2011-09-01 14:51:24 +0200

    Added Virtual datacenter creation example

---
+++ src/cloud.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env jython
+
+from com.abiquo.model.enumerator import *
+
+from com.apiclient.wrapper.admin import *
+from com.apiclient.wrapper.cloud import *
+from com.apiclient.connection import *
+
+
+# Virtual datacenter configuration
+VDC_NAME = "XS"
+VDC_TYPE = HypervisorType.XENSERVER
+
+# Network configuration
+NET_NAME = "Default Network"
+NET_ADDRESS = "192.168.1.0"
+NET_MASK = 24
+NET_GATEWAY = "192.168.1.1"
+
+
+def create_virtual_datacenter(datacenter, enterprise):
+    net = VLANNetwork(NET_NAME, NET_ADDRESS, NET_MASK, NET_GATEWAY, True)
+    vdc = VirtualDatacenter(datacenter, enterprise, VDC_NAME, VDC_TYPE, net)
+    vdc.save()
+    return vdc
+
+def create_cloud():
+    datacenter = ApiConnection.getConnection().getDatacenters()[0]
+    enterprise = ApiConnection.getConnection().getEnterprises()[0]
+
+    create_virtual_datacenter(datacenter, enterprise)
+
+
+if __name__ == '__main__':
+    create_cloud();
+
