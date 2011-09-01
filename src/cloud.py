5d647360a375f820defdf1c461a9ab6c9a4a04a6
Ignasi Barrera <ignasi.barrera@abiquo.com>
2011-09-01 15:29:36 +0200

    Added Virtual Appliance creation

--- src/cloud.py
+++ src/cloud.py
@@ -17,6 +17,9 @@ NET_ADDRESS = "192.168.1.0"
 NET_MASK = 24
 NET_GATEWAY = "192.168.1.1"
 
+# Virtual appliance configuration
+VAPP_NAME = "XS API"
+
 
 def create_virtual_datacenter(datacenter, enterprise):
     net = VLANNetwork(NET_NAME, NET_ADDRESS, NET_MASK, NET_GATEWAY, True)
@@ -24,11 +27,17 @@ def create_virtual_datacenter(datacenter, enterprise):
     vdc.save()
     return vdc
 
+def create_virtual_appliance(virtual_datacenter):
+    vapp = VirtualAppliance(virtual_datacenter, VAPP_NAME)
+    vapp.save()
+    return vapp
+
 def create_cloud():
     datacenter = ApiConnection.getConnection().getDatacenters()[0]
     enterprise = ApiConnection.getConnection().getEnterprises()[0]
 
-    create_virtual_datacenter(datacenter, enterprise)
+    vdc = create_virtual_datacenter(datacenter, enterprise)
+    create_virtual_appliance(vdc)
 
 
 if __name__ == '__main__':
