7cb09cff40500a509cf53f0753defc0a81a04dce
Ignasi Barrera <ignasi.barrera@abiquo.com>
2011-12-20 10:36:40 +0100

    Using the api-deploy model

--- src/constants.py
+++ src/constants.py
@@ -14,12 +14,12 @@ ABQ_ENDPOINT = "http://" + ABQ_ADDRESS + "/api"
 ## Infrastructure ##
 
 # Datacenter configuration
-DC_NAME = "Datacenter"          # The name of the datacenter
+DC_NAME = "Hawaii"              # The name of the datacenter
 DC_LOCATION = "Honolulu"        # The location of the datacenter
 DC_ADDRESS = ABQ_ADDRESS        # The address of the remote services
 
 # Rack configuration
-RACK_NAME = "API rack"
+RACK_NAME = "Coconut rack"
 RACK_VLAN_MIN = 3       # Minimum VLAN tag
 RACK_VLAN_MAX = 500     # Maximum VLAN tag
 RACK_NRSQ = 10          # VLAN pool size for VDC that exceed reservation
@@ -50,7 +50,7 @@ POOL_NAME = "abiquo"                    # The storage pool to use in the storage
 ## Enterprise ##
 
 # Enterprise configuration
-ENT_NAME = "API Enterprise"     # Enterprise name
+ENT_NAME = "Surfing Cloud"      # Enterprise name
 ENT_CPU_SOFT = 5                # Soft limut for the number of CPU
 ENT_CPU_HARD = 10               # Maximum CPU available
 ENT_RAM_SOFT = 2048             # Soft limit for the available RAM in MB
@@ -61,10 +61,10 @@ ENT_STORAGE_SOFT = 5120 * 1024 * 1024         # Soft limit for external storage 
 ENT_STORAGE_HARD = 10240 * 1024 * 1024        # Maximum available external storage in bytes
 
 # User configuration
-USR_ROLE = "ENTERPRISE_ADMIN"           # The role for the user
-USR_NAME = "Ignasi"                     # Name of the user
-USR_SURNAME = "Barrera"                 # Surname of the user
-USR_EMAIL = "ent-admin@abiquo.com"      # Surname of the user
-USR_LOGIN = "ibarrera"                  # The login name
-USR_PASS = "ibarrera"                   # The password
+USR_ROLE = "ENTERPRISE_ADMIN"               # The role for the user
+USR_NAME = "Enterprise"                     # Name of the user
+USR_SURNAME = "Administrator"               # Surname of the user
+USR_EMAIL = "eadmin@surfingcloud.com"       # Surname of the user
+USR_LOGIN = "eadmin"                        # The login name
+USR_PASS = "eadmin"                         # The password
 
