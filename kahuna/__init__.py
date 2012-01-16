908e9a9511a0531afe7a64bf06c91ac1ce14468e
Ignasi Barrera <ignasi.barrera@abiquo.com>
2012-01-16 18:13:49 +0100

    Close the classpath file after reading it

--- kahuna/__init__.py
+++ kahuna/__init__.py
@@ -1,12 +1,12 @@
 # Set the initial configuration needed to load the classpath
 # and the the first time we load the package 'kahuna'
+from __future__ import with_statement
 import sys
 
 # Set and parse the classpath with the jars we need to call
 # jclouds-abiquo methods
-f = open(__path__[0] + '/abiquo-jars.pth','r')
-kahuna_classpath = f.read().split(':')
-
-# Extend the classpath with the jars of the maven repository
-sys.path.extend(kahuna_classpath)
+with open(__path__[0] + '/abiquo-jars.pth','r') as f:
+    kahuna_classpath = f.read().split(':')
+    # Extend the classpath with the jars of the maven repository
+    sys.path.extend(kahuna_classpath)
 
