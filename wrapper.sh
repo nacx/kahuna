37b46e4aa2caf4ac231c46ca7fa942b967d51bab
Ignasi Barrera <ignasi.barrera@abiquo.com>
2012-01-17 17:58:57 +0100

    Wrapper script generates the classpath if missing

--- wrapper.sh
+++ wrapper.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 if [ $# -lt 1 ]; then
-    echo "Usage: ${0} <address> [<script>]"
+    echo "Usage: $0 <address> [<script>]"
     exit 1
 fi
 
+# Ensure the classpath file exist
+CPFILE=kahuna/abiquo-jars.pth
+! [ -f $CPFILE ] && mvn clean compile
+
 # Export the classpath and run the given script
 # or open a jython shell if no script arg was given
-export CLASSPATH=`cat kahuna/abiquo-jars.pth`
+export CLASSPATH=`cat $CPFILE`
 jython -Dabiquo.address=$*
