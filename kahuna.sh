aed0b6ef9875cab9bc261784d8d5856d8832ac1c
Ignasi Barrera <ignasi.barrera@abiquo.com>
2012-01-23 19:29:33 +0100

    Allow symlinking from the path

--- kahuna.sh
+++ kahuna.sh
@@ -3,6 +3,17 @@
 # Exit code to be used when opening an interactive shell
 EXIT_OPEN_SHELL=10
 
+# Allow creating symlinks in the path to point to this
+# script
+if [ -h $0 ]; then
+    BASEDIR=`readlink $0 | xargs dirname`
+else
+    BASEDIR=`DIRNAME $0`
+fi
+
+# Enter the real directori before performing any operation
+cd $BASEDIR
+
 # Ensure the classpath file exist
 CPFILE=kahuna/abiquo-jars.pth
 ! [ -f $CPFILE ] && mvn clean compile
--- kahuna/config.py
+++ kahuna/config.py
@@ -7,9 +7,11 @@ class Config:
     """ Main configuration. """
     def __init__(self):
         config = ConfigParser.SafeConfigParser()
-        file = '/etc/kahuna.conf'
+        file = '/usr/local/etc/kahuna.conf'
         if not os.path.exists(file):
-            raise IOError("Configuration file not found")
+            file = '/etc/kahuna.conf'
+            if not os.path.exists(file):
+                raise IOError("Configuration file not found")
         config.read(file)
         self.address = config.get("abiquo", "address")
         self.user = config.get("abiquo", "user")
