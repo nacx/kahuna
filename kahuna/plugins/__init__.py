0f1f0f1cdbd38dad521170e5605598550df1934f
Ignasi Barrera <ignasi.barrera@abiquo.com>
2012-01-24 14:49:37 +0100

    Automatically load plugins in the plugin directory

--- kahuna/plugins/__init__.py
+++ kahuna/plugins/__init__.py
@@ -1,5 +1,18 @@
 #!/usr/bin/env jython
 
-# List of all available plugins
-__all__ = ["env", "deployer", "vm", "shell"]
+import os
+
+# Automatically set the __all__ with all the available
+# plugins in the folder
+
+plugin_dir = "kahuna/plugins"
+
+__all__ = []
+for filename in os.listdir(plugin_dir):
+    filename = plugin_dir + "/" + filename
+    if os.path.isfile(filename):
+        basename = os.path.basename(filename)
+        base, extension = os.path.splitext(basename)
+        if extension == ".py" and not basename.startswith("_"):
+            __all__.append(base)
 
