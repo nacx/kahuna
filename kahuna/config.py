3ab1a49edfe67c714baf88177ff68486cd8f0c7c
Ignasi Barrera <ignasi.barrera@abiquo.com>
2012-01-27 17:47:51 +0100

    Generic configuration file loader
    Added generic configuration file loader to allow plugins
    be configured in their own configuration file in a generic way.

--- kahuna/config.py
+++ kahuna/config.py
@@ -9,39 +9,58 @@ from utils.singleton import singleton
 log = logging.getLogger('kahuna')
 log.setLevel(logging.INFO)
 
-@singleton
-class Config:
-    """ Main configuration. """
-    def __init__(self):
-        config = ConfigParser.SafeConfigParser() 
-        user_dir = os.environ['HOME'] + "/.kahuna"
-        user_config = user_dir + "/kahuna.conf"
+class ConfigLoader:
+    """ Loads configuration files from a given location. """
+    def __init__(self, basedir):
+        """ Initializes the ConfigLoader. """
+        self.user_dir = os.environ['HOME'] + "/." + basedir
+        self.sys_dir = "/etc/" + basedir
+
+    def load(self, file_name, default=None):
+        """ Loads the given configuration file ftom the default locations. """
+        user_config = self.user_dir + "/" + file_name
+        sys_config = self.sys_dir + "/" + file_name
         config_found = user_config
+
         # User config has precedence, then system
-        files = [user_config, '/etc/kahuna.conf']
+        files = [user_config, sys_config]
         for file in files:
             if os.path.exists(file):
                 log.debug("Config found in %s" % file)
                 config_found = file
                 break
 
-        # If no config file exists, copy the default one to the user config
         if not os.path.exists(config_found):
+            # Fail if config file is not found and do not want to create the default one
+            if not default:
+                raise IOError("Configuration file not found. " +
+                        "Please, make sure that %s or %s exist" % (user_config, sys_config));
+            # Create the default config file if nexessary
             log.warn("Kahuna config file not found. Creating the default one to %s" % user_config)
-            if not os.path.isdir(user_dir):
-                os.makedirs(user_dir)
-            shutil.copy('config/kahuna.conf', user_config)
+            if not os.path.isdir(self.user_dir):
+                os.makedirs(self.user_dir)
+            shutil.copy(default, user_config)
             config_found = user_config
 
+        config = ConfigParser.SafeConfigParser() 
         config.read(config_found)
+        return config
+
+@singleton
+class Config:
+    """ Main configuration. """
+    def __init__(self):
+        loader = ConfigLoader("kahuna")
+        config = loader.load("kahuna.conf", "config/kahuna.conf")
+
         self.address = config.get("connection", "address")
         self.user = config.get("connection", "user")
         self.password = config.get("connection", "pass")
-        
         try:
             self.loglevel = config.get("logging", "level")
             level = logging._levelNames[self.loglevel]
             log.setLevel(level)
-        except ConfigParser.NoOptionError:
+        except ConfigParser.NoOptionError:     
+            # Ignore errors if no logging level has been defined
             pass
 
