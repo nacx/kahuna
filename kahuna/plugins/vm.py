75809321e5ed77958e48a22e5613c154f68f5351
Ignasi Barrera <ignasi.barrera@abiquo.com>
2012-06-12 12:22:41 +0200

    Find by template merged into vm find command

--- kahuna/plugins/vm.py
+++ kahuna/plugins/vm.py
@@ -18,8 +18,6 @@ log = logging.getLogger('kahuna')
 
 class VmPlugin(AbsPlugin):
     """ Virtual machine plugin """
-    def __init__(self):
-        pass
 
     def list(self, args):
         """ List all virtual machines """
@@ -30,63 +28,46 @@ class VmPlugin(AbsPlugin):
         except (AbiquoException, AuthorizationException), ex:
             print "Error: %s" % ex.getMessage()
 
-
     def find(self, args):
         """ Find a virtual machine given its name """
         # Parse user input to get the name of the virtual machine
         parser = OptionParser(usage="vm find <options>")
         parser.add_option("-n", "--name", dest="name",
                 help="The name of the virtual machine to find")
-        parser.add_option("-v", "--verbose", dest="verbose",
-                action="store_true",
-                help="Show virtual machine extended information")
-        (options, args) = parser.parse_args(args)
-        name = options.name
-        if not name:
-            parser.print_help()
-            return
-
-        # Once user input has been read, find the virtual machine
-        try:
-            cloud = self._context.getCloudService()
-            vm = cloud.findVirtualMachine(VirtualMachinePredicates.name(name))
-            if vm:
-                pprint_vms([vm], options.verbose)
-            else:
-                print "No virtual machine found with name: %s" % name
-        except (AbiquoException, AuthorizationException), ex:
-            print "Error: %s" % ex.getMessage()
-
-    def find_by_template(self, args):
-        """ Find virtual machine(s) given a template """
-        # Parse user input to get the name of the virtual machine
-        parser = OptionParser(usage="vm find_by_template <options>")
         parser.add_option("-t", "--template", dest="template",
                 help="The name of the template")
         parser.add_option("-v", "--verbose", dest="verbose",
                 action="store_true",
                 help="Show virtual machine extended information")
         (options, args) = parser.parse_args(args)
+        name = options.name
         template = options.template
-        if not template:
+        if not name and not template:
             parser.print_help()
             return
 
         # Once user input has been read, find the virtual machine
         try:
             cloud = self._context.getCloudService()
-            vms = cloud.listVirtualMachines()
-            vmts = []
-            for vm in vms:
-                if template in vm.getTemplate().getName():
-                    vmts.append(vm)
-            if not vmts:
-                print "No virtual machine found with template: %s" % template
-            else:
-                pprint_vms(vmts,options.verbose)
+            if name:  # Find by name
+                vm = cloud.findVirtualMachine(
+                        VirtualMachinePredicates.name(name))
+                if vm:
+                    pprint_vms([vm], options.verbose)
+                else:
+                    print "No virtual machine found with name: %s" % name
+            else:  # Find by template
+                vms = cloud.listVirtualMachines()
+                vmts = filter(lambda vm: template in
+                        vm.getTemplate().getName(), vms)
+                if len(vmts) == 0:
+                    print ("No virtual machine found "
+                        "matching template: %s" % template)
+                else:
+                    pprint_vms(vmts, options.verbose)
+
         except (AbiquoException, AuthorizationException), ex:
             print "Error: %s" % ex.getMessage()
-
 
     def deploy(self, args):
         """ Deploy an existing virtual machine given its name """
