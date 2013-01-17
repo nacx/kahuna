b0f4240c09389eb452dfc8a9a59feaf85e0b690a
Ignasi Barrera <ignasi.barrera@abiquo.com>
2013-01-17 23:06:28 +0100

    Added simple web server to provide indirect upload

--- kahuna/plugins/template.py
+++ kahuna/plugins/template.py
@@ -4,6 +4,10 @@ from kahuna.abstract import AbsPlugin
 from kahuna.utils.prettyprint import pprint_templates
 from org.jclouds.abiquo.domain.exception import AbiquoException
 from org.jclouds.rest import AuthorizationException
+from optparse import OptionParser
+from threading import Thread
+from time import sleep
+from upload.repository import TransientRepository
 
 
 class TemplatePlugin(AbsPlugin):
@@ -18,6 +22,28 @@ class TemplatePlugin(AbsPlugin):
             pprint_templates(templates)
         except (AbiquoException, AuthorizationException), ex:
             print "Error: %s" % ex.getMessage()
+
+    def upload(self, args):
+        """ Upload a template to a repository """
+        parser = OptionParser(usage="template upload <options>")
+        parser.add_option('-p', '--port',
+                help='The port to open locally for the repository',
+                action='store', dest='port', type='int', default=8888)
+        (options, args) = parser.parse_args(args)
+
+        repo = TransientRepository()
+        try:
+            repo.create()
+            thread = Thread(target=repo.start, args=[options.port])
+            thread.start()
+
+            # TODO: Once the server is listening, call the API to download
+            # the necessary files.
+            sleep(5)
+        except (AbiquoException, AuthorizationException), ex:
+            print "Error: %s" % ex.getMessage()
+        finally:
+            repo.destroy()
 
 
 def load():
---
+++ kahuna/plugins/upload/repository.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env jython
+
+import logging
+import os
+import random
+import select
+import shutil
+import string
+import SimpleHTTPServer
+import SocketServer
+
+log = logging.getLogger("kahuna")
+
+
+class TransientRepository:
+    """ A transient tempalte repository """
+    def __init__(self, basedir="/tmp"):
+        """ Sets the base directory to serve """
+        rnd = ''.join(random.choice(string.letters) for i in xrange(5))
+        self._repodir = basedir + '/repo-' + rnd
+        self.__server = None
+
+    def create(self):
+        """ Creates the repository structure """
+        if not os.path.exists(self._repodir):
+            os.makedirs(self._repodir)
+
+        # TODO Add all required arguments to this method to populate
+        # the contents of the repository
+
+    def destroy(self):
+        """ Destroys the repository and its contents """
+        if self.__server:
+            self.stop()
+        shutil.rmtree(self._repodir)
+
+    def start(self, port=8888):
+        """ Start serving the contents of the repository """
+        handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+        self.__server = RepositoryServer(("localhost", port), handler)
+        log.debug("Serving repository at %s in port %s" %
+            (self._repodir, port))
+        os.chdir(self._repodir)
+        self.__server.serve_until_stopped()
+
+    def stop(self):
+        """ Stop serving the contents of the repository """
+        if self.__server:
+            log.debug("Shutting down repository server")
+            self.__server.shutdown()
+            self.__server = None
+
+
+class RepositoryServer(SocketServer.TCPServer):
+    """ Extends the TCP server to add the shutdown missing in Python 2.5 """
+    def serve_until_stopped(self):
+        """ Starts listening for requests """
+        self.socket.setblocking(0)
+        self.serving = True
+        while self.serving:
+            read, write, exception = select.select([self], [], [], 0.5)
+            if read:
+                self.handle_request()
+
+    def shutdown(self):
+        """ Stops listening for requests """
+        self.serving = False
